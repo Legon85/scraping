85e94464bd9c5d9473d4abbd95eb6fd880d51557
Legon85 <zre12f@gmail.com>
2024-04-05 11:58:31 +0300

    some files added

--- o_O.py
+++ o_O.py
@@ -2,49 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-list_card_url = []
-
 headers = {"User-Agent":
-               "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (-NET CLR 3.5.30729)"}
+               "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (-NET CLR "
+               "3.5.30729)"}
 
-for count in range(1, 7):
+def download(url):
+    resp = requests.get(url, stream=True)
+    r = open("C:\\Python_work_Legion\\venv_projects\\scraping\\image\\" + url.split("/")[-1], "wb")
+    for value in resp.iter_content(1024*1024):
+        r.write(value)
+    r.close()
 
-    sleep(3)
-    url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
+def get_url():
+    for count in range(1, 7):
 
-    response = requests.get(url, headers=headers)
+        url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")  # html.parser
+        data = soup.find_all("div", class_="w-full rounded border")
 
-    soup = BeautifulSoup(response.text, "lxml")  # html.parser
-
-    data = soup.find_all("div", class_="w-full rounded border")
-
-    for i in data:
-        card_url = "https://scrapingclub.com" + i.find("a").get("href")
-        list_card_url.append(card_url)
-
-
-
-
-
-
-
+        for i in data:
+            card_url = "https://scrapingclub.com" + i.find("a").get("href")
+            yield card_url
 
 
+def array():
+    for card_url in get_url():
+        response = requests.get(card_url, headers=headers)
+        # sleep(3)
+        soup = BeautifulSoup(response.text, "lxml")  # html.parser
 
-
-
-
-
-
-
-
-
-
-
-
-
-        # name = i.find("h4").text.replace("\n", "")
-        # price = i.find("h5").text
-        # url_img = "https://scrapingclub.com" + i.find("img", class_="card-img-top img-fluid").get("src")
-        #
-        # print(name + "\n" + price + "\n" + url_img + "\n\n")
+        data = soup.find("div", class_="my-8 w-full rounded border")
+        name = data.find("h3", class_="card-title").text
+        price = data.find("h4", class_="my-4 card-price").text
+        text = data.find("p", class_='card-description').text
+        url_img = "https://scrapingclub.com" + data.find("img", class_="card-img-top").get("src")
+        download(url_img)
+        yield name, price, text, url_img
